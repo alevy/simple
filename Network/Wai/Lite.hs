414be48901dfad695c2a322e42716f19c28c5a71
Amit Aryeh Levy <amit@amitlevy.com>
2012-10-29 22:53:22 -0400

    Added lite wrapper module and bump minor version

---
+++ Network/Wai/Lite.hs
@@ -0,0 +1,10 @@
+module Network.Wai.Lite (
+    module Network.Wai.Router
+  , module Network.Wai.Responses
+  , module Network.Wai.Controller
+  ) where
+
+import Network.Wai.Router
+import Network.Wai.Responses
+import Network.Wai.Controller
+
