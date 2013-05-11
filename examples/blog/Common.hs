c423a264436794d4e9dc2714420b7e59161c03ef
Amit Aryeh Levy <amit@amitlevy.com>
2013-05-10 22:41:03 -0700

    Database pool

--- examples/blog/Common.hs
+++ examples/blog/Common.hs
@@ -2,17 +2,40 @@
 module Common where
 
 import Control.Monad.IO.Class
+import qualified Data.ByteString.Char8 as S8
 import Data.Pool
-import qualified Database.PostgreSQL.Connection as DB
 import Database.PostgreSQL.Simple
+import System.Environment
 import Web.Simple.Controller
 
 data AppSettings = AppSettings { appDB :: Pool Connection }
 
-withConnection :: AppSettings -> (Connection -> Controller a) -> Controller a
-withConnection = DB.withConnection . appDB
-
 newAppSettings :: IO AppSettings
 newAppSettings = do
-  DB.createConnectionPool Nothing >>= return . AppSettings
+  createConnectionPool Nothing >>= return . AppSettings
+
+createConnectionPool :: Maybe S8.ByteString -> IO (Pool Connection)
+createConnectionPool mconnectStr = do
+  createPool (createConnection mconnectStr)
+             (\c -> rollback c >> close c)
+             1
+             (fromInteger 60)
+             20
+
+createConnection :: Maybe S8.ByteString -> IO Connection
+createConnection mconnectStr = do
+  dbUrl <- case mconnectStr of
+    Nothing -> do
+      env <- getEnvironment
+      return $ maybe S8.empty S8.pack $ lookup "DATABASE_URL" env
+    Just str -> return str
+  connectPostgreSQL dbUrl
+
+withConnection :: AppSettings
+               -> (Connection -> Controller b) -> Controller b
+withConnection settings func = withResource (appDB settings) $ \conn -> do
+  liftIO $ begin conn
+  res <- func conn
+  liftIO $ commit conn
+  return res
 
