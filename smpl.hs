41728c09f6e8584e8bf5f19e50a749cc43fa6a99
Amit Aryeh Levy <amit@amitlevy.com>
2013-08-18 13:27:09 -0700

    Smplify and templetize smpl

--- smpl.hs
+++ smpl.hs
@@ -4,12 +4,17 @@
 module Main (main) where
 
 import Prelude hiding (writeFile, FilePath)
+import Control.Monad
 import qualified Data.ByteString.Char8 as S8
-import Network.Wai.Handler.DevelServer (runQuit)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy.Search as L8
 import System.Console.CmdArgs
 import System.Directory
 import System.FilePath
 import System.Environment
+import System.Process
+
+import Paths_simple
 
 data Smpl =
     Server
@@ -21,8 +26,8 @@ data Smpl =
 
 main :: IO ()
 main = do
-    env <- getEnvironment
-    let myport = maybe 3000 read $ lookup "PORT" env
+    myenv <- getEnvironment
+    let myport = maybe 3000 read $ lookup "PORT" myenv
     let develMode = cmdArgsMode $ modes
                   [ Server { port = myport &= typ "PORT"
                            , moduleName = "Main" &= typ "MODULE"
@@ -31,20 +36,23 @@ main = do
                   , Create { appName = "" &= argPos 0 &= typ "APP_NAME" } ]
     smpl <- cmdArgsRun develMode
     case smpl of
-      Server p m -> do
-        putStrLn $ "Starting server at 0.0.0.0:" ++ (show myport)
-        runQuit p m "app" (const $ return [])
+      Server p m -> void $
+        rawSystem "wai-handler-devel" [show p, m, "app"]
       Create myAppName -> createApplication myAppName
 
 createApplication :: String -> IO ()
 createApplication myAppName = do
-  createDirectoryIfMissing True $ myAppName </> "db" </> "migrations"
-  S8.writeFile (myAppName </> "Main.hs") mainTemplate
+  createDirectory myAppName
+  let mappings = [("pkgname", myAppName)]
+  copyTemplate ("template" </> "Main_hs.tmpl")
+               (myAppName </> "Main.hs") mappings
+  copyTemplate ("template" </> "package_cabal.tmpl")
+               (myAppName </> myAppName ++ ".cabal") mappings
 
-mainTemplate :: S8.ByteString
-mainTemplate = S8.concat
-  [ "{-# LANGUAGE OverloadedStrings #-}\n\n"
-  , "module Main where\n\n"
-  , "import Web.Simple\n\n"
-  , "app runner = runner $ mkRouter $ okHtml \"Hello World\"\n\n"]
+copyTemplate :: FilePath -> FilePath -> [(String, String)] -> IO ()
+copyTemplate orig target mappings = do
+  tmpl <- getDataFileName orig >>= L8.readFile
+  L8.writeFile target $
+    (flip . flip foldl) tmpl mappings $ \accm (key, val) ->
+      L8.replace (S8.pack $ "{{" ++ key ++ "}}") (L8.pack val) accm
 
