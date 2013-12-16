78be2d2ddc495a82c1098d716a6b468cb12467b9
Amit Aryeh Levy <amit@amitlevy.com>
2013-12-15 23:57:22 -0500

    No logging for main

--- blog-example/Main.hs
+++ blog-example/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import System.Environment
 import Network.Wai.Handler.Warp
-import Network.Wai.Middleware.RequestLogger
 
 import Application
 
@@ -10,5 +9,5 @@ main :: IO ()
 main = do
   env <- getEnvironment
   let port = maybe 3000 read $ lookup "PORT" env
-  app (run port . logStdout)
+  app (run port)
 
